27047c0eba37dd117de289c3be8d7fce08564882
zhouql <3279106972@qq.com>
2023-10-09 21:56:02 +0800

    fix:初始化项目文件结构，vue打包配置等

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  productionSourceMap: false,// 生产环境是否要生成 sourceMap
+  publicPath: './',  //   部署应用包时的基本 URL
+  assetsDir: 'assets',  //   放置静态文件夹目录
 })
